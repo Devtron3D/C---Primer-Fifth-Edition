b061973a349d9752c6ee38a89590a51968b84b3e
Devtron <mobile@3dbyte.com>
2017-01-24 16:44:28 +0100

    done

---
+++ Chapter 01/ex1.6/ex1.6/Source.cpp
@@ -0,0 +1,9 @@
+// Exercise 1.6
+
+// std::cout << "The sum of " << v1;
+//           << " and " << v2;
+//			 << " is " << v1 + v2 << std::endl;
+
+//  The program is not legal, as the expression is eneded after ; 
+//  so the last two lines of code will not be executed;
+//	This will also not compile;
