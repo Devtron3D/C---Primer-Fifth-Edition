430fdd1072bb574172e428b1de1abef9f4c35d09
Devtron <mobile@3dbyte.com>
2017-01-24 18:30:33 +0100

    done

---
+++ Chapter 01/ex1.8/ex1.8/Source.cpp
@@ -0,0 +1,10 @@
+// Exercise 1.8
+
+
+	std::cout << "/*";
+	std::cout << "*/";
+	std::cout << /* "*/" */;
+	std::cout << /* "*/" /* "/*" */;
+
+// the first and second should work
+// the third and fourth will not
