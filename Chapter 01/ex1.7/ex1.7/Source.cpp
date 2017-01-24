fba33bbae2d6138bf767500950926b6be526129d
Devtron <mobile@3dbyte.com>
2017-01-24 18:26:45 +0100

    done

---
+++ Chapter 01/ex1.7/ex1.7/Source.cpp
@@ -0,0 +1,16 @@
+// Exercise 1.7
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	/*
+	std::cout << "Remember to comment code. using // or /* ... */" << std::endl;
+	*/
+	return 0;
+
+
+
+
+}
